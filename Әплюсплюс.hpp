34ef614d350a9d3944f0b6cb1dfdce7b6e1257e3
xi816 <141260554+xi816@users.noreply.github.com>
2023-11-10 22:43:14 +0300

    Add files via upload

---
+++ Әплюсплюс.hpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+
+// Подключение сети имён
+#define куярга using
+#define исемләр namespace
+
+
+// Ввод-вывод
+#define Татарлар std
+
+#define карагыз cout
+#define тәртипсез cerr
+#define әйтегез cin
+#define икесен_алып_эшләргә swap
+
+#define юл_бетте endl
+
+
+// Объявление переменных
+#define башларга int main
+#define сан int
+#define нокталы float
+#define ике_нокталы double
+
+#define бер_яклы unsigned
+#define кирәкмәгән_нәрсә void
+#define компьютерның_көче size_t
+#define зур long
+#define кечкенә short
+
+#define үзем_эшләдем template
+#define исеме_аның typename
+
+#define бирергә return
+#define яхшы 0
+#define ялгыш 1
+
+#define яңа new
+#define кирәкми delete
+#define БЕРНИ NULL
+#define белмим_кайда nullptr
+#define ниндидер random
+#define монысына_тимәскә const
+#define бетте_баш throw
+#define программаң_тупой exception
+
+
+// Условия, циклы
+#define булса if
+#define бу_булса else if
+#define юк_бит else
+#define була ?
+#define булганда while
+#define бөтенесенә for
+
+#define бетерергә break
+#define монысы_кирәкми continue
+#define шул_булса switch
+#define мәсәлән case
+#define юк_бит_шундыйлар default
+
+
+// ООП
+#define класс class
+#define структура struct
+#define сан_пар enum
+
+#define аныкы_гына private
+#define дусларына protected
+#define бөтен_кешегә public
+
